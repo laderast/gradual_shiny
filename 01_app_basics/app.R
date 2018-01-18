ca3317e8996104acdf8a6235432553694541fe02
laderast <roshioshi@gmail.com>
2018-01-17 21:27:47 -0800

    first commit

---
+++ 01_app_basics/app.R
@@ -0,0 +1,57 @@
+##01 - Shiny App Basics
+##Here we'll learn how to connect server and ui together with the input and output objects
+#
+## loading code here
+## anything you load here can be seen by both ui and server
+
+library(shiny)
+library(tidyverse)
+library(fivethirtyeight)
+
+##load the biopics data
+data(biopics)
+biopics <- biopics %>% filter(!is.na(box_office))
+##specify what categories we want to color with
+select_color_options <- c("type_of_subject", "subject_race", "subject_sex")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel(""),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+        selectInput("color_opts", "Select Category to Color With",
+                    choices = select_color_options)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("scatter_plot") #,
+        # plotOutput("boxoffice_boxplot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$scatter_plot <- renderPlot({
+
+      biopics %>% ggplot(aes_string(y="box_office", x="year_release", color=input$color_opts)) + 
+       geom_point()
+     
+   })
+   
+   output$boxoffice_boxplot <- renderPlot({
+     
+     biopics %>% ggplot(aes_string(x="type_of_subject", y="box_office")) + geom_boxplot()
+     
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
---
+++ readme.md
@@ -0,0 +1,19 @@
+## Shiny Workshop For PDX Rlang Meetup
+
+Welcome to the workshop! We'll introduce you to the basics behind Shiny, an easy to use web application framework. We'll learn the basics by building a data exploration app piece by piece.
+
+01) Getting the basics down
+02) What are reactives for?
+03) Tooltips and graph interactivity
+04) Observe/isolate/update
+05) Where to go next
+
+## Requirements
+
+You will need a laptop with RStudio installed. Additionally, you will need the following packages installed:
+
+```
+install.packages("fivethirtyeight", "shiny", "tidyverse")
+```
+
+To get started, clone or download this repo and open the `01_app_basics` folder.
