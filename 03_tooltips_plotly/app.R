40a749d5f46843919040e441159d1106945b9c71
Ted Laderas <roshioshi@gmail.com>
2021-06-03 17:52:27 -0700

    update of repo and site to use .data[[]]

--- 03_tooltips_plotly/app.R
+++ 03_tooltips_plotly/app.R
@@ -2,15 +2,15 @@ library(shiny)
 library(fivethirtyeight)
 library(ggplot2)
 library(plotly)
-
 data(biopics)
+
 categoricalVars <- c("country", "type_of_subject", "subject_race", "subject_sex")
 
 ui <- fluidPage(
   plotlyOutput("movie_plot"),
   selectInput(inputId = "color_select", 
               label = "Select Categorical Variable", 
-              choices = categoricalVars)
+              choices = categoricalVars, selected = categoricalVars[1])
 )
 
 server <- function(input, output) {
@@ -18,20 +18,20 @@ server <- function(input, output) {
   output$movie_plot <- renderPlotly({
     
     my_plot <- ggplot(biopics) +
-      aes_string(x = "box_office", 
-                 y="year_release",
-                 color=input$color_select,
-                 title="title",
-                 director="director",
-                 box_office="box_office", 
-                 subject="subject") +
+      aes(x = box_office, 
+          y=year_release,
+          color= .data[[input$color_select]],
+          title=title,
+          director=director,
+          box_office=box_office, 
+          subject=subject) +
       geom_point() +
       theme(legend.position="none")
     
     ##notice we pass our ggplot into ggplotly, which makes it more interactive
     ggplotly(my_plot)
   })
-
+  
 }
 
 shinyApp(ui, server)
